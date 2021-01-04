344a8a9e949182c8807aaffa684ba2c09dccf825
Alexson Coelho <coelho@Alexsons-MacBook-Pro.local>
2021-01-04 14:58:18 -0500

    projects page

--- src/components/Post.js
+++ src/components/Post.js
@@ -38,7 +38,7 @@ export const Post = () => {
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
           {postData &&
             postData.map((post, index) => (
-              <article>
+              <article key={index}>
                 <Link to={"post" + post.slug.current} key={post.slug.current}>
                   <span
                     className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400"
