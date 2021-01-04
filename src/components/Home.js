564aa600ea5e5ccf99e2a5c44e3744c3a6660b8e
Alexson Coelho <coelho@Alexsons-MacBook-Pro.local>
2021-01-04 12:05:59 -0500

    post view

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import React from "react";
 
 export const Home = () => {
-    return <h1>Home</h1>
-}
+  return (
+    <main className="bg-red-50">
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-black-100 font-bold cursive leading-none lg:leading-snug home-name">
+          Hello. I'm Alexson
+        </h1>
+      </section>
+    </main>
+  );
+};
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import {SocialIcon} from 'react-social-icons';
 
 export const NavBar = () => {
     return (
-        <header className="bg-red-600">
+        <header className="bg-black">
             <div className="container mx-auto flex justify-between">
                 <nav className="flex">
                     <NavLink to='/' exact 
